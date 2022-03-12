3e3da52181a53e98e3ba44072743df7731e28e7d
KulakDmitry <kulakdima@mail.ru>
2022-03-12 13:46:23 +0300

    Updates

---
+++ static/js/907.55ed6e45.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_practice=self.webpackChunkreact_practice||[]).push([[907],{907:function(e,c,r){r.r(c),r.d(c,{default:function(){return n}});r(791);var t=r(184);function n(){return(0,t.jsx)("div",{children:"hello"})}}}]);
+//# sourceMappingURL=907.55ed6e45.chunk.js.map
