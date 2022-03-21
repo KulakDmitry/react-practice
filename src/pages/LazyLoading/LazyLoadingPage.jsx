5c86dfb0fe9d726e85a53d2b5a31c386b7ee2676
KulakDmitry <kulakdima@mail.ru>
2022-03-21 15:32:47 +0300

    removed unuse code

--- src/pages/LazyLoading/LazyLoadingPage.jsx
+++ src/pages/LazyLoading/LazyLoadingPage.jsx
@@ -4,14 +4,8 @@ const LoadingComponent = lazy(() => import("../LazyLoading/LoadingComponent"));
 
 export default function LazyLoadingPage() {
   return (
-    <div>
-      <Suspense
-        fallback={<div className="text-6xl text-center">Loading...</div>}
-      >
-        <div className="App">
-          <LoadingComponent />
-        </div>
-      </Suspense>
-    </div>
+    <Suspense fallback={<div className="text-6xl text-center">Loading...</div>}>
+      <LoadingComponent />
+    </Suspense>
   );
 }
