de07ab3b0ddf59afc6e4413ebcd0650b7ea061d5
KulakDmitry <kulakdima@mail.ru>
2022-04-18 16:06:49 +0300

    rename tests, add new test, move const inside describe

--- src/utils/getNumberOfRaceRunners/getNumberOfRaceRunners.test.js
+++ src/utils/getNumberOfRaceRunners/getNumberOfRaceRunners.test.js
@@ -1,14 +1,16 @@
 import getNumberOfRaceRunners from "./getNumberOfRaceRunners";
 
-const runners = [
-  { name: "Unnamed Favourite" },
-  { name: "Den" },
-  { name: "John" },
-];
-
 describe("getNumberOfRaceRunners", () => {
-  it("should return count of runners", () => {
-    expect(getNumberOfRaceRunners(runners)).toBe(2);
+  const mockArrayWithRunners = [
+    { name: "Unnamed Favourite" },
+    { name: "Den" },
+    { name: "John" },
+    { name: "Carl", nonRunner: true },
+    { name: "Mike", nonRunner: false },
+  ];
+
+  it("should return count of runners if array with runners is provided", () => {
+    expect(getNumberOfRaceRunners(mockArrayWithRunners)).toBe(3);
   });
 
   it("should return 0 if use invalid name", () => {
@@ -17,5 +19,15 @@ describe("getNumberOfRaceRunners", () => {
 
   it("should return 0 if use emptey array", () => {
     expect(getNumberOfRaceRunners([])).toBe(0);
+  });
+
+  it("should return 0 if nonRunners is true", () => {
+    expect(getNumberOfRaceRunners([{ name: "Carl", nonRunner: true }])).toBe(0);
+  });
+
+  it("should return 1 if nonRunners is false", () => {
+    expect(getNumberOfRaceRunners([{ name: "Mike", nonRunner: false }])).toBe(
+      1
+    );
   });
 });
